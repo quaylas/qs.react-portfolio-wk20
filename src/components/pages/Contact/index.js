d98eb01e06e34c8608cae05a00ea7ae7cd259c73
quaylas <quayla@cxadvisors.com>
2021-04-04 21:39:47 -0600

    added contact form

--- src/components/pages/Contact/index.js
+++ src/components/pages/Contact/index.js
@@ -1,12 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Contact () {
+    const [formState, setFormState] = useState({ fullName: '', email: '', message: '' });
+    const { fullName, email, message } = formState;
+
+    function handleChange(e){
+        setFormState({ ...formState, [e.target.name]: e.target.value })
+    }
+
+    function handleSubmit(e){
+        e.preventDefault();
+        console.log(formState);
+    };
+
     return (
         <section className="about-me-section grid-outer">
         <h2>Contact Me</h2>
-        <div className="contact-me-content grid-content">
-            THIS IS A CONTACT FORM.
+        <form id="contact-form" className="contact-me-content grid-content" onSubmit={handleSubmit} >
+            <div>
+                <input type="text" name="fullName" placeholder="Name" defaultValue={fullName} onChange={handleChange} />
             </div>
+            <div>
+                <input type="email" name="email" placeholder="Email Address" defaultValue={email} onChange={handleChange} />
+            </div>
+            <div>
+                <textarea name="message" rows="5" placeholder="Message" defaultValue={message} onChange={handleChange} />
+            </div>
+            <button type='submit'>Submit</button>
+        </form>
         </section>
     );
 }
